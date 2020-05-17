2a0fd81797952ed50c1c4fedafb5e284201c849b
shivesh98 <shiveshs98@gmail.com>
2020-05-17 22:51:04 +0530

    Initial Commit

---
+++ frontend/src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Dashboard from './todos/Dashboard';
+import Header from './layout/Header';
+import { Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../store';
+import history from '../history';
+import TodoDelete from './todos/TodoDelete';
+import TodoEdit from './todos/TodoEdit';
+
+
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <Router history={history}>
+                    <Header />
+                    <Switch>
+                        <Route exact path="/" component={Dashboard} />
+                        <Route exact path="/delete/:id" component={TodoDelete} />
+                        <Route exact path='/edit/:id' component={TodoEdit} />
+                    </Switch>
+                </Router>
+            </Provider>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
